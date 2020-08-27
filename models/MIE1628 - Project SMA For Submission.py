3c8f9e5d9ad17b1459d078ed2d4fc5ad0870f345
b2le <69889867+b2le@users.noreply.github.com>
2020-08-27 18:32:58 -0400

    Add files via upload

--- models/MIE1628 - Project SMA For Submission.py
+++ models/MIE1628 - Project SMA For Submission.py
@@ -18,6 +18,7 @@
 # MAGIC from pyspark.sql import *
 # MAGIC from pyspark.sql.functions import *
 # MAGIC from pyspark.sql.window import Window
+# MAGIC import pyspark.sql.functions as func
 
 # COMMAND ----------
 
@@ -93,8 +94,7 @@ canada_21_21 = sma_plot(21, 21, "canada_cases", 87)
 # DBTITLE 1,SMAPE Function
 # MAGIC %python
 # MAGIC 
-# MAGIC import pyspark.sql.functions as func
-# MAGIC 
+# MAGIC # Equation from -> https://en.wikipedia.org/wiki/Symmetric_mean_absolute_percentage_error
 # MAGIC def SMAPE(Country_Query_Prediction, Country):
 # MAGIC   SMAPE = Country_Query_Prediction.withColumn("SMAPE_num", abs(col("Prediction")-col(Country)))
 # MAGIC   SMAPE = SMAPE.withColumn("SMAPE_den", (abs(col("Prediction"))+abs(col(Country)))/1) #previously it was divide by 2 but the group decided this factor was not needed to use the alternative formula for SMAPE.
@@ -183,6 +183,53 @@ def RMSE_table(Country):
 
 # COMMAND ----------
 
+# R^2 Functions
+# Equation from -> https://en.wikipedia.org/wiki/Coefficient_of_determination
+def R2(Country_Query_Prediction, Country):
+  R2 = Country_Query_Prediction
+  R2_avg = R2.groupby().avg(Country).collect()[0][0]
+  R2 = R2.withColumn("R2_TSS", (col(Country)-R2_avg)**2)
+  R2 = R2.withColumn("R2_RSS", (col(Country)-col("Prediction"))**2)
+  R2_value = (1 - (R2.groupby().sum("R2_RSS").collect()[0][0])/(R2.groupby().sum("R2_TSS").collect()[0][0]))*100 # Multiply by 100 for percentage
+  return R2_value
+
+def R2_table(Country):
+  # Different parameters for moving average
+  result_1_1 = sma(1, 1, Country, 87)
+  result_7_1 = sma(7, 1, Country, 87)
+  result_14_1 = sma(14, 1, Country, 87)
+  result_21_1 = sma(21, 1, Country, 87)
+  result_1_7 = sma(1, 7, Country, 87)
+  result_7_7 = sma(7, 7, Country, 87)
+  result_14_7 = sma(14, 7, Country, 87)
+  result_21_7 = sma(21, 7, Country, 87)
+  result_1_14 = sma(1, 14, Country, 87)
+  result_7_14 = sma(7, 14, Country, 87)
+  result_14_14 = sma(14, 14, Country, 87)
+  result_21_14 = sma(21, 14, Country, 87)
+  result_1_21 = sma(1, 21, Country, 87)
+  result_7_21 = sma(7, 21, Country, 87)
+  result_14_21 = sma(14, 21, Country, 87)
+  result_21_21 = sma(21, 21, Country, 87)
+
+  # Creating a dataframe to make a table to summarize the results
+  Results = Row("Model", "1 Day", "7 Day", "14 Day", "21 Day")
+  results1 = Results("1 Day Prediction", R2(result_1_1, Country), R2(result_7_1, Country), R2(result_14_1, Country), R2(result_21_1, Country))
+  results2 = Results("7 Day Prediction", R2(result_1_7, Country), R2(result_7_7, Country), R2(result_14_7, Country), R2(result_21_7, Country))
+  results3 = Results("14 Day Prediction", R2(result_1_14, Country), R2(result_7_14, Country), R2(result_14_14, Country), R2(result_21_14, Country))
+  results4 = Results("21 Day Prediction", R2(result_1_21, Country), R2(result_7_21, Country), R2(result_14_21, Country), R2(result_21_21, Country))
+  df = spark.createDataFrame([results1, results2, results3, results4])
+  df = df.withColumn(
+    "1 Day", func.round(df["1 Day"], 2)).withColumn(
+    "7 Day", func.round(df["7 Day"],2)).withColumn(
+    "14 Day", func.round(df["14 Day"],2)).withColumn(
+    "21 Day", func.round(df["21 Day"],2))
+  print(Country)
+  df.show()
+
+
+# COMMAND ----------
+
 # DBTITLE 1,RMSE Results
 RMSE_table("canada_cases")
 RMSE_table("japan_cases")
@@ -200,3 +247,13 @@ SMAPE_table("italy_cases")
 SMAPE_table("UK_cases")
 SMAPE_table("germany_cases")
 SMAPE_table("france_cases")
+
+# COMMAND ----------
+
+# DBTITLE 1,R2 Results
+R2_table("canada_cases")
+R2_table("japan_cases")
+R2_table("italy_cases")
+R2_table("UK_cases")
+R2_table("germany_cases")
+R2_table("france_cases")
